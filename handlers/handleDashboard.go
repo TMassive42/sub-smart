b4ea038c1f3207c32b9bdefdc7b10b889a1acb52
moonyango <mosesadrian825@gmail.com>
2024-12-14 11:55:13 +0300

    feat: handle dashboard

---
+++ handlers/handleDashboard.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func (app *App) HandleDashboard(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/dashboard.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
